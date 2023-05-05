2db3402fa5aeae2f6669203ca8e958c29c3caafd
mehpatel79 <123248785+mehpatel79@users.noreply.github.com>
2023-05-05 18:59:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import FilterWithHooks from "./filter-with-hooks/filter-with-hooks";
+
+function App() {
+
+  return <FilterWithHooks />
+    
+}
+
+export default App;
+
+
+
